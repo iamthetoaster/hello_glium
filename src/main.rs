b5fb300b4b9bab9b80de029c977928b7d920e183
Iamthetoaster <14080501+iamthetoaster@users.noreply.github.com>
2020-02-20 13:40:44 -0800

    oops I committed without adding anything

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,38 @@ extern crate glium;
 extern crate image;
 
 mod sphere_gen;
-
-use rtriangulate::{TriangulationPoint, triangulate};
+use rand::prelude::*;
 
 fn main() {
     use glium::{glutin, Surface};
 
     let mut events_loop = glium::glutin::EventsLoop::new();
     let wb = glium::glutin::WindowBuilder::new();
-    let cb = glium::glutin::ContextBuilder::new();
+    let cb = glium::glutin::ContextBuilder::new().with_depth_buffer(24);
     let display = glium::Display::new(wb, cb, &events_loop).unwrap();
 
     #[derive(Copy, Clone)]
     struct Vertex {
-        position: [f32; 2],
+        position: [f32; 3],
         col: [f32; 4]
     }
 
     implement_vertex!(Vertex, position, col);
     
 
-    let sphere = sphere_gen::sphere(40);
-    let sphere = sphere.into_iter().map(|x| x);
+    let ico = sphere_gen::icosahedron();
 
     let mut shape: Vec<Vertex> = Vec::new();
+
+    let mut rng = thread_rng();
+    for tri in ico {
+        let red: f32 = rng.gen();
+        let green: f32 = rng.gen();
+        let blue: f32 = rng.gen();
+        for point in &tri {
+            shape.push( Vertex { position: *point, col: [red, green, blue, 0.7] });
+        }
+    }
 
     
 
@@ -36,14 +44,19 @@ fn main() {
     let vertex_shader_src = r#"
         #version 140
 
-        in vec2 position;
+        in vec3 position;
         in vec4 col;
         out vec4 colr;
-        uniform mat4 matrix;
+        uniform mat4 translate;
+        uniform mat4 scale;
+        uniform mat4 xRotation;
+        uniform mat4 yRotation;
+        uniform mat4 zRotation;
+        uniform mat4 perspective;
 
         void main() {
-            colr = col;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
+            gl_Position = translate * yRotation * xRotation * zRotation * scale * vec4(position, 1.0);
+            colr = col * ((-gl_Position.z + 1) / 2);
         }
     "#;
 
@@ -64,21 +77,105 @@ fn main() {
     let mut t: f32 = 0.0;
     let mut closed = false;
     while !closed {
-        t += 0.0004;
-        let uniforms = uniform!{
-            matrix: [
-                [ t.cos(), t.sin(), 0.0, 0.0],
-                [-t.sin(), t.cos(), 0.0, 0.0],
-                [     0.0,     0.0, 1.0, 0.0],
-                [     0.0,     0.0, 0.0, 1.0f32]
+        t += 0.0006;
+
+        let mut target = display.draw();
+        target.clear_color_and_depth((0.7, 0.7, 1.0, 1.0), 1.0);
+
+        let translate_vector = [0.0, 0.0, 0.0f32];
+
+        let scale_vector = [0.7, 0.7, 0.7f32];
+
+        let rotation_vector = [0.5 * t, t, 0.25 * t];
+
+        let perspective = {
+            let (width, height) = target.get_dimensions();
+            let aspect_ratio = height as f32 / width as f32;
+        
+            let fov: f32 = std::f32::consts::PI / 6.0;
+            let zfar = 2048.0;
+            let znear = 0.1;
+        
+            let f = 1.0 / (fov / 2.0).tan();
+        
+            [
+                [f *   aspect_ratio   ,    0.0,              0.0              ,   0.0],
+                [         0.0         ,     f ,              0.0              ,   0.0],
+                [         0.0         ,    0.0,  (zfar+znear)/(zfar-znear)    ,   1.0],
+                [         0.0         ,    0.0, -(2.0*zfar*znear)/(zfar-znear),   0.0],
             ]
         };
-        let mut target = display.draw();
-        target.clear_color(0.7, 0.7, 1.0, 1.0);
 
+        let translate = {
+            let [x, y, z] = translate_vector;
+            [
+                [1.0, 0.0, 0.0,   x],
+                [0.0, 1.0, 0.0,   y],
+                [0.0, 0.0, 1.0,   z],
+                [0.0, 0.0, 0.0, 1.0f32]
+            ]
+        };
+
+        let scale = {
+            let [x, y, z] = scale_vector;
+            [
+                [  x, 0.0, 0.0, 0.0],
+                [0.0,   y, 0.0, 0.0],
+                [0.0, 0.0,   z, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32],
+            ]
+        };
+
+        let y_rotation = {
+            let ang = rotation_vector[1];
+            [
+                [ ang.cos(), 0.0, ang.sin(), 0.0],
+                [       0.0, 1.0,       0.0, 0.0],
+                [-ang.sin(), 0.0, ang.cos(), 0.0],
+                [       0.0, 0.0,       0.0, 1.0f32]   
+            ]
+        };
+
+        let x_rotation = {
+            let ang = rotation_vector[0];
+            [
+                [1.0,       0.0,        0.0, 0.0],
+                [0.0, ang.cos(), -ang.sin(), 0.0],
+                [0.0, ang.sin(),  ang.cos(), 0.0],
+                [0.0,       0.0,        0.0, 1.0f32]
+            ]
+        };
+
+        let z_rotation = {
+            let ang = rotation_vector[2];
+            [
+                [ang.cos(), -ang.sin(), 0.0, 0.0],
+                [ang.sin(),  ang.cos(), 0.0, 0.0],
+                [       0.0,       0.0, 1.0, 0.0],
+                [       0.0,       0.0, 0.0, 1.0f32]   
+            ]
+        };
+
+        let uniforms = uniform!{
+            perspective: perspective,
+            translate: translate,
+            scale: scale, 
+            yRotation: y_rotation,
+            xRotation: x_rotation,
+            zRotation: z_rotation
+        };
+
+        let params = glium::DrawParameters {
+            depth: glium::Depth {
+                test: glium::draw_parameters::DepthTest::IfLess,
+                write: true,
+                .. Default::default()
+            },
+            .. Default::default()
+        };
 
         target.draw(
-            &vertex_buffer, &indices, &program, &uniforms, &Default::default()
+            &vertex_buffer, &indices, &program, &uniforms, &params
         ).unwrap();
         target.finish().unwrap();
 
--- src/sphere_gen/mod.rs
+++ src/sphere_gen/mod.rs
@@ -24,98 +24,47 @@ pub fn sphere(number_of_points: usize) -> Vec<[f32; 3]> {
     return result;
 }
 
-fn square_distance(a: [f32; 3], b: [f32; 3]) -> f32{
-    (a[0] - b[0]).powi(2) +
-    (a[1] - b[1]).powi(2) +
-    (a[2] - b[2]).powi(2)
+pub fn icosahedron() -> Vec<[[f32; 3]; 3]> {
+    let pts = [
+        [0.0, 0.0, 0.0],
+        [0.000000, -0.525731, 0.850651],
+        [0.850651, 0.000000, 0.525731],
+        [0.850651, 0.000000, -0.525731],
+        [-0.850651, 0.000000, -0.525731],
+        [-0.850651, 0.000000, 0.525731],
+        [-0.525731, 0.850651, 0.000000],
+        [0.525731, 0.850651, 0.000000],
+        [0.525731, -0.850651, 0.000000],
+        [-0.525731, -0.850651, 0.000000],
+        [0.000000, -0.525731, -0.850651],
+        [0.000000, 0.525731, -0.850651],
+        [0.000000, 0.525731, 0.850651]
+    ];
+    let ico = vec! {
+        [pts[2], pts[3], pts[7]],
+        [pts[2], pts[8], pts[3]],
+        [pts[4], pts[5], pts[6]],
+        [pts[5], pts[4], pts[9]],
+        [pts[7], pts[6], pts[12]],
+        [pts[6], pts[7], pts[11]],
+        [pts[10], pts[11], pts[3]],
+        [pts[11], pts[10], pts[4]],
+        [pts[8], pts[9], pts[10]],
+        [pts[9], pts[8], pts[1]],
+        [pts[12], pts[1], pts[2]],
+        [pts[1], pts[12], pts[5]],
+        [pts[7], pts[3], pts[11]],
+        [pts[2], pts[7], pts[12]],
+        [pts[4], pts[6], pts[11]],
+        [pts[6], pts[5], pts[12]],
+        [pts[3], pts[8], pts[10]],
+        [pts[8], pts[2], pts[1]],
+        [pts[4], pts[10], pts[9]],
+        [pts[5], pts[9], pts[1]],
+    };
+    return ico;
 }
 
-// credit to wikipedia https://en.wikipedia.org/wiki/Bowyer%E2%80%93Watson_algorithm
-pub fn delaunay(points: &Vec<[f32; 3]>) -> Vec<[[f32;3]; 3]> {
-    let (
-        mut max_x, mut max_y, mut max_z, 
-        mut min_x, mut min_y, mut min_z
-    ) = (
-        std::f32::NEG_INFINITY, std::f32::NEG_INFINITY, std::f32::NEG_INFINITY, 
-        std::f32::INFINITY, std::f32::INFINITY, std::f32::INFINITY
-    );
-
-    for point in points {
-        max_x = max_x.max(point[0]);
-        max_y = max_y.max(point[1]);
-        max_z = max_z.max(point[2]);
-        min_x = min_x.min(point[0]);
-        min_y = min_y.min(point[1]);
-        min_z = min_z.min(point[2]);
-    }
-
-    max_x += 1.0; max_y += 1.0; max_z += 1.0;
-    min_x -= 1.0; min_y -= 1.0; min_z -= 1.0;
-
-    let super_triangle = [
-        [min_x, min_y, min_z],
-        [min_x, max_y, min_z],
-        [max_x, max_y, max_z]
-    ];
-
-    let mut triangulation: Vec<[[f32;3]; 3]> = vec!(super_triangle);
-    
-    for point in points {
-        let mut bad_triangles: Vec<[[f32;3]; 3]> = Vec::new();
-        let mut bad_triangle_indicies: Vec<usize> = Vec::new();
-        let mut index = 0_usize;
-        for triangle in &triangulation {
-            let (mut center_x, mut center_y, mut center_z) = (0_f32, 0_f32, 0_f32);
-            for corner in triangle {
-                center_x += corner[0];
-                center_y += corner[1];
-                center_z += corner[2];
-            }
-            center_x /= 3.0;
-            center_y /= 3.0;
-            center_z /= 3.0;
-            let center = [center_x, center_y, center_z];
-            if square_distance(center, *point) < square_distance(center, triangle[0]) {
-                bad_triangles.push(*triangle);
-                bad_triangle_indicies.push(index);
-            }
-            index += 1;
-        }
-        let mut polygon: Vec<[[f32; 3]; 2]> = vec!();
-        for triangle in bad_triangles {
-            for i in 0..triangle.len() {
-                let a = triangle[i];
-                let b = triangle[(i + 1) % 3];
-                if !(polygon.contains(&[a, b]) || polygon.contains(&[b, a])){
-                    polygon.push([a, b])
-                }
-            }
-        }
-        let mut i = 0;
-        for index in 0..bad_triangle_indicies.len() {
-            triangulation.remove(index - i);
-            i += 1;
-        }
-        for edge in polygon {
-            let new_triangle = [edge[0], edge[1], *point];
-            triangulation.push(new_triangle);
-        }
-    }
-    let mut supers: Vec<usize> = Vec::new();
-    for i in 0..triangulation.len() {
-        for corner_1 in &triangulation[i] {
-            for corner_2 in &super_triangle {
-                if corner_1 == corner_2 {
-                    supers.push(i);
-                }
-            }
-        }
-    }
-    let mut i = 0;
-    for j in supers {
-        triangulation.remove(j - i);
-        i += 1;
-    }
-
-    return triangulation;
+pub fn icosphere(subdivisions: usize) -> Vec<[[f32; 3]; 3]> {
+    return icosahedron();
 }
